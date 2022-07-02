2947ee5e4727ac1e423413eaee9b071293d8cc68
fluxink <pavelvolk300@gmail.com>
2022-07-02 14:15:58 +0300

    part 3

--- django_blog_tutorial/blog/views.py
+++ django_blog_tutorial/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from matplotlib.style import context
+
+
+posts = [
+    {
+        'author': 'FluxInk',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '2 July 2022'
+    },
+    {
+        'author': 'CripsyWood',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '2 July 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
