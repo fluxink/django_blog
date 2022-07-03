2bf976da5d963cb86c60f57c35573d088c9d295b
fluxink <pavelvolk300@gmail.com>
2022-07-03 19:37:18 +0300

    user pictures and profile? I forgot...

--- django_blog_tutorial/django_blog_tutorial/settings.py
+++ django_blog_tutorial/django_blog_tutorial/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -129,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
