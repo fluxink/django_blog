9bd39fdd50bb9eba40dfa9f2e7918c62e895ad9e
fluxink <pavelvolk300@gmail.com>
2022-07-03 11:38:37 +0300

    startapp Users, and create register view with Crispy-forms

--- django_blog_tutorial/blog/views.py
+++ django_blog_tutorial/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from matplotlib.style import context
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'FluxInk',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': '2 July 2022'
-    },
-    {
-        'author': 'CripsyWood',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': '2 July 2022'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
