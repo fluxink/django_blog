fd7ba14090af452e8cb1d3faec72fdbcf98f3653
fluxink <pavelvolk300@gmail.com>
2022-07-05 10:11:44 +0300

    profile update

--- django_blog_tutorial/users/signals.py
+++ django_blog_tutorial/users/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+import os
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -11,3 +12,22 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(pre_save, sender=Profile)
+def auto_delete_image_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_image = Profile.objects.get(pk=instance.pk).image
+    except Profile.DoesNotExist:
+        return False
+    new_image = instance.image
+    if not old_image == new_image and 'default.png' not in old_image.path:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
